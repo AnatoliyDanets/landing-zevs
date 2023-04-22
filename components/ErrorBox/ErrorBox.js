2b4eece992b3c077998f8ab33307de5bf52b6513
AnatoliyDanets <danetsxp@gmail.com>
2023-04-22 16:11:29 +0300

    added "Not found " page

---
+++ components/ErrorBox/ErrorBox.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Container from "../Container/Container";
+import s from "./ErrorBox.module.css";
+
+export default function ErrorBox() {
+    return (
+        <Container>
+            <section className={s.error}>
+                <h1 className={s.error__title}>Zevs is not here :(</h1>
+                <Link className={s.error__link} href="/">
+                    Back to Zevs
+                </Link>
+            </section>
+        </Container>
+    );
+}
---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import ErrorBox from "@/components/ErrorBox/ErrorBox";
+
+export default function Error() {
+    return <ErrorBox />;
+}
