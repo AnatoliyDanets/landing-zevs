bd37f52b9c6b2a4f5f4ddb4307981ab34387a7c1
AnatoliyDanets <danetsxp@gmail.com>
2023-07-02 22:33:05 +0300

    fixed section"Popular Product"

--- components/PopularProducts/PopularProducts.js
+++ components/PopularProducts/PopularProducts.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import PropTypes from "prop-types";
 import useFormattedDate from "@/hook/hook";
@@ -21,6 +22,27 @@ export const ConvertToNormalDate = ({ date }) => {
 };
 
 export default function DiscountCards({ products, locale }) {
+    const [popularProducts, setPopularProducts] = useState([]);
+    useEffect(() => {
+        if (products?.length > 1) {
+            setPopularProducts(
+                products
+                    .sort((a, b) => a.size - b.size)
+                    .reduce(
+                        (acc, el) => (
+                            acc.find(({ model }) => el.model[locale] === model[locale]) ||
+                            acc.push(el),
+                            acc
+                        ),
+                        []
+                    )
+                    .sort(() => {
+                        return Math.random() - 0.5;
+                    })
+            );
+        }
+    }, [products]);
+
     return (
         <Section id={"Discount"}>
             <Container>
@@ -48,85 +70,72 @@ export default function DiscountCards({ products, locale }) {
                         modules={[Autoplay, Pagination, Navigation]}
                         className={s.slider}
                     >
-                        {products
-                            .reduce(
-                                (acc, el) => (
-                                    acc.find(({ model }) => el.model[locale] === model[locale]) ||
-                                    acc.push(el),
-                                    acc
-                                ),
-                                []
-                            )
+                        {popularProducts.map((el, i) => (
+                            <SwiperSlide key={i}>
+                                <div className={s.discount__item}>
+                                    <Image
+                                        src={makeSizeForImage(el.cardImg[0].url, 450, 450)}
+                                        alt={`discount ${el.model[locale]}`}
+                                        width="450"
+                                        height="450"
+                                        className={s.discount__img}
+                                        // priority
+                                        blurDataURL="data:..."
+                                        placeholder="blur"
+                                    />
+                                    {el.discount > 0 && (
+                                        <div className={s.discount__time}>
+                                            <FormattedMessage id="page.home.discount_product_time" />
+                                            {<ConvertToNormalDate date={el.discount_time} />}
+                                        </div>
+                                    )}
 
-                            .map((el, i) => (
-                                <SwiperSlide key={i}>
-                                    <div className={s.discount__item}>
-                                        <Image
-                                            src={makeSizeForImage(el.cardImg[0].url, 450, 450)}
-                                            alt={`discount ${el.model[locale]}`}
-                                            width="450"
-                                            height="450"
-                                            className={s.discount__img}
-                                            // priority
-                                            blurDataURL="data:..."
-                                            placeholder="blur"
-                                        />
-                                        {el.discount > 0 && (
-                                            <div className={s.discount__time}>
-                                                <FormattedMessage id="page.home.discount_product_time" />
-                                                {<ConvertToNormalDate date={el.discount_time} />}
-                                            </div>
-                                        )}
-
-                                        <article className={s.discount__info}>
-                                            <h3 className={s.discount__title}>{el.model[locale]}</h3>
-                                            <div className={s.popular__price}>
-                                                <p
-                                                    className={s.discount__price}
-
-                                                >
-                                                    <span className={s.discount__price_text}>від</span>{" "}
-                                                    {el.discount > 0
-                                                        ? el.price - (el.discount / 100) * el.price
-                                                        : el.price}{" "}
-                                                    грн
-                                                </p>
-                                                {el.discount > 0 && (
-                                                    <div className={s.discount__price__wrapper}>
-                                                        <p className={s.discount__price_discount}>
-                                                            {el.price} грн
-                                                        </p>
-                                                    </div>
-                                                )}
-                                            </div>
+                                    <article className={s.discount__info}>
+                                        <h3 className={s.discount__title}>{el.model[locale]}</h3>
+                                        <div className={s.popular__price}>
+                                            <p className={s.discount__price}>
+                                                <span className={s.discount__price_text}>
+                                                    <FormattedMessage id="page.home.discount_text_from" />
+                                                </span>{" "}
+                                                {el.discount > 0
+                                                    ? el.price - (el.discount / 100) * el.price
+                                                    : el.price}{" "}
+                                                грн
+                                            </p>
                                             {el.discount > 0 && (
-                                                <div>
-                                                    <div
-                                                        style={{ display: "flex", alignItems: "center" }}
-                                                    >
-                                                        <span className={s.discount__discount}>
-                                                            {" "}
-                                                            -{el.discount}{" "}
-                                                            <span style={{ fontSize: "12px" }}>%</span>
-                                                        </span>{" "}
-                                                    </div>
+                                                <div className={s.discount__price__wrapper}>
+                                                    <p className={s.discount__price_discount}>
+                                                        {el.price} грн
+                                                    </p>
                                                 </div>
                                             )}
-                                            <Link
-                                                className={s.discount__btn}
-                                                to={el.model[locale]}
-                                                href={`#${el.model[locale]}`}
-                                                spy={true}
-                                                smooth={true}
-                                                offset={-50}
-                                                duration={500}
-                                            >
-                                                <FormattedMessage id="page.home.discount_product_link" />
-                                            </Link>
-                                        </article>
-                                    </div>
-                                </SwiperSlide>
-                            ))}
+                                        </div>
+                                        {el.discount > 0 && (
+                                            <div>
+                                                <div style={{ display: "flex", alignItems: "center" }}>
+                                                    <span className={s.discount__discount}>
+                                                        {" "}
+                                                        -{el.discount}{" "}
+                                                        <span style={{ fontSize: "12px" }}>%</span>
+                                                    </span>{" "}
+                                                </div>
+                                            </div>
+                                        )}
+                                        <Link
+                                            className={s.discount__btn}
+                                            to={el.model[locale]}
+                                            href={`#${el.model[locale]}`}
+                                            spy={true}
+                                            smooth={true}
+                                            offset={-50}
+                                            duration={500}
+                                        >
+                                            <FormattedMessage id="page.home.discount_product_link" />
+                                        </Link>
+                                    </article>
+                                </div>
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
                 </div>
             </Container>
