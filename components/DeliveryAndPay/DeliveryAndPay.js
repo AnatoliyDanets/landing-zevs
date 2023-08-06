7a8e2ee57af896d2179dc8f218ced574442df583
AnatoliyDanets <danetsxp@gmail.com>
2023-08-06 16:15:56 +0300

    added new property for background and changed text  in component "DeliveryAndPay"

--- components/DeliveryAndPay/DeliveryAndPay.js
+++ components/DeliveryAndPay/DeliveryAndPay.js
@@ -1,4 +1,5 @@
 import { FormattedMessage } from "react-intl";
+import Link from "next/link";
 import Section from "../Section/Section";
 import Container from "../Container/Container";
 import SectionTitle from "../SectionTitle/SectionTitle";
@@ -23,42 +24,41 @@ export default function DeliveryAndPay() {
                             <FormattedMessage id="page.home.delivery_mail_one_item_two" />
                         </li>
                     </ul>
+                    <div className={s.delivery__btns}>
+                        <Link
 
+                            className={s.delivery__btn}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={"https://novaposhta.ua/basic_tariffs"}
+
+                        >
+                            <FormattedMessage id="page.home.delivery_mail_one_item_btn" />
+                        </Link>
+                    </div>
                     <h3 className={s.delivery__title}>
                         <FormattedMessage id="page.home.delivery_title_two" />
                     </h3>
                     <ul className={s.delivery__list}>
                         <li className={s.delivery__condition}>
-                            <p className={s.delivery__postal}>
-                                <FormattedMessage id="page.home.delivery_mail_two_item_one" />
-                            </p>
-                            <ul className={s.delivery__list}>
-                                <li className={s.delivery__condition}>
-                                    <FormattedMessage id="page.home.delivery_mail_two_item_one_one" />
-                                </li>
-                                <li>
-                                    {" "}
-                                    <FormattedMessage id="page.home.delivery_mail_two_item_one_two" />
-                                </li>
-                            </ul>
+                            <FormattedMessage id="page.home.delivery_mail_two_item_one" />
                         </li>
-
                         <li className={s.delivery__condition}>
-                            <p className={s.delivery__postal}>
-                                {" "}
-                                <FormattedMessage id="page.home.delivery_mail_two_item_two" />
-                            </p>
-                            <ul className={s.delivery__list}>
-                                <li className={s.delivery__condition}>
-                                    <FormattedMessage id="page.home.delivery_mail_two_item_two_one" />
-                                </li>
-                                <li className={s.delivery__condition}>
-                                    <FormattedMessage id="page.home.delivery_mail_two_item_two_two" />
-                                </li>
-                            </ul>
+                            <FormattedMessage id="page.home.delivery_mail_two_item_two" />
                         </li>
                     </ul>
+                    <div className={s.delivery__btns}>
+                        <Link
 
+                            className={s.delivery__btn}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={"https://www.ukrposhta.ua/ua/taryfy-ukrposhta-standart"}
+
+                        >
+                            <FormattedMessage id="page.home.delivery_mail_two_item_btn" />
+                        </Link>
+                    </div>
                     <h3 className={s.delivery__title}>
                         <FormattedMessage id="page.home.delivery_title_three" />
                     </h3>
